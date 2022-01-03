16cb6e76609952bad82dcdbfb291808d9717f972
Allan Casabar <allan.casabar@gmail.com>
2022-01-04 00:12:17 +0800

    Create first_greater.py

---
+++ python/binary_search/first_greater.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+def first_not_smaller(arr: List[int], target: int) -> int:
+    left, right = 0, len(arr) - 1
+    boundary_index = -1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] >= target:
+            boundary_index = mid
+            right = mid - 1
+        else:
+            left = mid + 1
+    return boundary_index
+
+
+if __name__ == '__main__':
+    arr = [int(x) for x in "1 3 3 5 8 8 10".split()]
+    print(arr)
+    target = 2
+    res = first_not_smaller(arr, target)
+    print(res)

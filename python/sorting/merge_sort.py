6d62014db5650b1ab90adc14cf66a59a84ad3000
Allan Casabar <allan.casabar@gmail.com>
2022-01-16 09:46:03 +0800

    Create merge_sort.py

---
+++ python/sorting/merge_sort.py
@@ -0,0 +1,47 @@
+from typing import List
+
+# python is pass by reference
+def merge_sort(array: List[int]):
+    if len(array) > 1:
+
+        mid = len(array) // 2
+
+        left = array[:mid]
+        right = array[mid:]
+
+        print("input: {}".format(array))
+        print("left: {}".format(left))
+        print("right: {}".format(right))
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i = j = k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                array[k] = left[i]
+                i += 1
+            else:
+                array[k] = right[j]
+                j += 1
+            k += 1
+
+        # Checking if any element was left
+        while i < len(left):
+            array[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            array[k] = right[j]
+            j += 1
+            k += 1
+
+        print("sorted: {}".format(array))
+
+
+if __name__ == "__main__":
+    array: List[int] = [5, 3, 1, 2, 4]
+    merge_sort(array)
+    print(array)
